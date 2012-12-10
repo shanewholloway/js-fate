0a8cea95d3e4eed368e22b4a207d5ddfddbd4f72
Shane Holloway <shane.holloway@ieee.org>
2012-12-10 10:22:46 -0700

    Standardied on ';' usage

--- fate.js
+++ fate.js
@@ -12,48 +12,48 @@ For futures and promises {then:, resolve:, reject:} must always be fully bound c
 
 "use strict";
 
-exports.Promise = Promise;
+exports.Promise = Promise
 function Promise(then) { if (then!==undefined) this.then = then }
 Promise.api = {
-    always: function(always) { return this.then(always, always); },
-    done: function(success) { return this.then(success, undefined); },
-    fail: function(failure) { return this.then(undefined, failure); },
+    always: function(always) { return this.then(always, always) },
+    done: function(success) { return this.then(success, undefined) },
+    fail: function(failure) { return this.then(undefined, failure) },
     timeout: function(ms) { return Promise.timeout(this, ms) } }
 Promise.prototype = Object.create(Promise.api,
-    {promise: {get: function(){return this}}});
+    {promise: {get: function(){return this}}})
 
-exports.isPromise = Promise.isPromise = isPromise;;
+exports.isPromise = Promise.isPromise = isPromise
 function isPromise(tgt) {
-    return (tgt!==undefined) && (tgt.then!==undefined); }
+    return (tgt!==undefined) && (tgt.then!==undefined) }
 
-exports.asPromise = Promise.wrap = asPromise;
+exports.asPromise = Promise.wrap = asPromise
 function asPromise(tgt) {
-    return isPromise(tgt) ? tgt : Future.resolved(tgt).promise; }
+    return isPromise(tgt) ? tgt : Future.resolved(tgt).promise }
 
-exports.when = Promise.when = when;
+exports.when = Promise.when = when
 function when(tgt, success, failure) {
     if (success!==undefined || failure!==undefined)
         return asPromise(tgt).then(success, failure)
-    else return asPromise(tgt); }
+    else return asPromise(tgt) }
 
-exports.Future = Future;
+exports.Future = Future
 function Future(then, resolve, reject) {
-    this.promise = new Promise(then);
+    this.promise = new Promise(then)
     if (resolve !== undefined)
-        this.resolve = resolve;
+        this.resolve = resolve
     if (reject !== undefined)
-        this.reject = reject;
+        this.reject = reject
 }
 Future.prototype = Object.create(Promise.api,
-    {then: {get: function(){return this.promise.then}}});
+    {then: {get: function(){return this.promise.then}}})
 Future.prototype.resolve = function(result) { }
 Future.prototype.reject = function(error) { }
 
-exports.isFuture = Future.isFuture = isFuture;
+exports.isFuture = Future.isFuture = isFuture
 function isFuture(tgt) {
     return (tgt!==undefined) &&
          ( (typeof tgt.resolve==='function')
-        || (typeof tgt.reject ==='function')); }
+        || (typeof tgt.reject ==='function')) }
 
 
 //~ Future: thenable, deferred, resolved and rejected ~~~~~~~
@@ -61,84 +61,84 @@ exports.thenable = Future.thenable = thenable
 function thenable(thisArg, success, failure, inner) {
     if (typeof success === "object") {
         if (failure===undefined)
-            failure = success.failure || success.reject || success.fail;
-        success = success.success || success.resolve || success.done;
+            failure = success.failure || success.reject || success.fail
+        success = success.success || success.resolve || success.done
     }
     if (success===undefined && failure===undefined)
         return Future.deferred(thisArg)
-    else return new Future(then, resolve, reject);
+    else return new Future(then, resolve, reject)
 
     function then(success, failure) {
         if (inner===undefined)
             inner = Future.deferred(thisArg)
-        return inner.then(success, failure); }
+        return inner.then(success, failure) }
     function resolve(result) {
-        var next = (inner!==undefined) ? inner : Future.absentTail;
+        var next = (inner!==undefined) ? inner : Future.absentTail
         if (success!==undefined)
             try {
                 var res = success.call(thisArg, result)
-                if (res!==undefined) result = res;
+                if (res!==undefined) result = res
             } catch (err) {
-                success = failure = undefined;
-                inner = Future.rejected(err);
-                return next.reject(err);
+                success = failure = undefined
+                inner = Future.rejected(err)
+                return next.reject(err)
             }
-        else if (failure===undefined) return;
-        success = failure = undefined;
-        inner = Future.resolved(result);
-        return next.resolve(result); }
+        else if (failure===undefined) return
+        success = failure = undefined
+        inner = Future.resolved(result)
+        return next.resolve(result) }
     function reject(error) {
-        var next = (inner!==undefined) ? inner : Future.absentTail;
+        var next = (inner!==undefined) ? inner : Future.absentTail
         if (failure!==undefined)
             try {
                 var res = failure.call(thisArg, error)
-                if (res!==undefined) error = res;
-            } catch (err) { error = err; }
+                if (res!==undefined) error = res
+            } catch (err) { error = err }
         else if (success===undefined) return;
-        success = failure = undefined;
-        inner = Future.rejected(error);
-        return next.reject(error); } }
+        success = failure = undefined
+        inner = Future.rejected(error)
+        return next.reject(error) } }
 
 Future.absentTail = {resolve: function(result) {}, reject: function(error) {}}
 Future.onActionError = function(error, thisArg) { console.error(error) }
 
-exports.deferred = Future.deferred = deferred;
+exports.deferred = Future.deferred = deferred
 function deferred(thisArg) {
-    var actions=[], inner;
-    return new Future(then, resolve, reject);
+    var actions=[], inner
+    return new Future(then, resolve, reject)
     function then(success, failure) {
         if (inner!==undefined)
-            return inner.then(success, failure);
-        var ans = Future.thenable(thisArg, success, failure);
-        actions.push(ans);
-        return ans.promise; }
+            return inner.then(success, failure)
+        var ans = Future.thenable(thisArg, success, failure)
+        actions.push(ans)
+        return ans.promise }
     function resolve(result) {
         if (actions===undefined) return;
-        inner = Future.resolved(result, thisArg);
+        inner = Future.resolved(result, thisArg)
         for (var i=0; i<actions.length; i++) {
             var ea = actions[i].resolve
             if (ea===undefined) continue
             try { ea.call(thisArg, result)
             } catch (err) { Future.onActionError(err, thisArg) }
-        } actions = undefined; }
+        } actions = undefined }
     function reject(error) {
         if (actions===undefined) return
-        inner = Future.rejected(error, thisArg);
+        inner = Future.rejected(error, thisArg)
         for (var i=0; i<actions.length; i++) {
             var ea = actions[i].reject
             if (ea===undefined) continue
             try { ea.call(thisArg, error)
             } catch (err) { Future.onActionError(err, thisArg) }
-        } actions = undefined; }
+        } actions = undefined }
 }
 
-exports.resolved = Future.resolved = resolved;
+exports.resolved = Future.resolved = resolved
 function resolved(result, thisArg) {
     return new Future(function then(success, failure) {
         var ans = thenable(thisArg, success, failure)
         return ans.resolve(result), ans.promise })}
 
-exports.rejected = Future.rejected = rejected;
+exports.rejected = Future.rejected = rejected
 function rejected(error, thisArg) {
     return new Future(function then(success, failure) {
         var ans = thenable(thisArg, success, failure)
@@ -146,22 +146,22 @@ function rejected(error, thisArg) {
 
 
 //~ Utility Futures: invert, delay and timeout ~~~~~~~~~~~~~~
-exports.inverted = Future.inverted = inverted;
+exports.inverted = Future.inverted = inverted
 function inverted(tgt) {
-    if (tgt===undefined) tgt = deferred();
-    return new Future(tgt.then, tgt.reject, tgt.resolve) };
+    if (tgt===undefined) tgt = deferred()
+    return new Future(tgt.then, tgt.reject, tgt.resolve) }
 
-exports.delay = Future.delay = Promise.delay = delay;
+exports.delay = Future.delay = Promise.delay = delay
 function delay(ms, bReject) {
     var res=deferred(),
-        tid=setTimeout(bReject?res.reject:res.resolve, ms);
-    res.always(function() {clearTimeout(tid)});
-    return res; }
-exports.timeout = Future.timeout = Promise.timeout = timeout;
+        tid=setTimeout(bReject?res.reject:res.resolve, ms)
+    res.always(function() {clearTimeout(tid)})
+    return res }
+exports.timeout = Future.timeout = Promise.timeout = timeout
 function timeout(target, ms, bReject) {
-    var res = delay(ms, (bReject!=undefined?bReject:true));
-    when(target, res);
-    return res; }
+    var res = delay(ms, (bReject!=undefined?bReject:true))
+    when(target, res)
+    return res }
 
 
 //~ Compositions: any, all, every, first ~~~~~~~~~~~~~~~~~~~~
@@ -178,28 +178,24 @@ function forEachPromise(anArray, thisArg, resolveFirst, rejectFirst, rejectAll) 
                 future.resolve(anArray) },
         reject: function() {
             if (rejectFirst || (--n < 1))
-                future.reject(anArray);
+                future.reject(anArray)
             if (rejectAll)
-                future.resolve = future.reject;} };
+                future.resolve = future.reject } }
 
-    [].forEach.call(anArray, linchpin.push)
-    if (n<1) future.resolve(n);
-    return future; }
+    ;[].forEach.call(anArray, linchpin.push)
+    if (n<1) future.resolve(n)
+    return future }
 
-exports.every = Future.every = Promise.every = every;
+exports.every = Future.every = Promise.every = every
 function every(anArray, thisArg) {
-    if (arguments.length>1) anArray = [].slice.call(arguments);
     return forEachPromise(anArray, thisArg, false, false, true) }
-exports.all = Future.all = Promise.all = all;
+exports.all = Future.all = Promise.all = all
 function all(anArray, thisArg) {
-    if (arguments.length>1) anArray = [].slice.call(arguments);
     return forEachPromise(anArray, thisArg, false, true) }
-exports.first = Future.first = Promise.first = first;
+exports.first = Future.first = Promise.first = first
 function first(anArray, thisArg) {
-    if (arguments.length>1) anArray = [].slice.call(arguments);
     return forEachPromise(anArray, thisArg, true, true) }
-exports.any = Future.any = Promise.any = any;
+exports.any = Future.any = Promise.any = any
 function any(anArray, thisArg) {
-    if (arguments.length>1) anArray = [].slice.call(arguments);
     return forEachPromise(anArray, thisArg, true, false) }
 
